3f3965c122b2f59a9d0bf214f6e5713a2405b69a
jvcl <valdi.jorge@gmail.com>
2015-08-14 17:52:58 -0300

    license added to Helper class

--- src/main/java/com/flyonet/wpjava/Helper.java
+++ src/main/java/com/flyonet/wpjava/Helper.java
@@ -7,8 +7,22 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
- * Created by jorge on 14/08/15.
+ * This file is part of WP-JAVA.
+ *
+ *  WP-JAVA is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  WP-JAVA is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with WP-JAVA.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 public class Helper {
 
     public static String getJSON(String url) throws IOException {
