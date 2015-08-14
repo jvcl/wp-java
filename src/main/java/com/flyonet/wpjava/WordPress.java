078f2947f00993699bbd6282343f3f3f7fa874d2
jvcl <valdi.jorge@gmail.com>
2015-08-14 17:55:49 -0300

    variables, getters and setters added to Wordpress class

--- src/main/java/com/flyonet/wpjava/WordPress.java
+++ src/main/java/com/flyonet/wpjava/WordPress.java
@@ -18,4 +18,37 @@ package com.flyonet.wpjava;
  */
 
 public class WordPress {
+    private String url;
+    private String username;
+    private String password;
+
+    public WordPress(String url, String username, String password) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
