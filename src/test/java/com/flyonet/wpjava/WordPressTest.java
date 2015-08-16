65c7fcd09585b8dd3e18bce3a1cd1383b6949995
jvcl <valdi.jorge@gmail.com>
2015-08-15 22:15:45 -0300

    test cases added

--- src/test/java/com/flyonet/wpjava/WordPressTest.java
+++ src/test/java/com/flyonet/wpjava/WordPressTest.java
@@ -1,6 +1,9 @@
 package com.flyonet.wpjava;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Random;
 
 import static org.junit.Assert.*;
 
@@ -8,10 +11,39 @@ import static org.junit.Assert.*;
  * Created By jorge 14/08/15
  */
 public class WordPressTest {
+    WordPress wp;
+    @Before
+    public void setUp() throws Exception {
+        wp = new WordPress("http://localhost:8888/", "admin", "admin");
+
+    }
 
     @Test
     public void testFixURL() throws Exception {
-        WordPress wp = new WordPress("http://wp-api.org/", "test", "test");
-        assertEquals("http://wp-api.org", wp.getUrl());
+        assertEquals("http://localhost:8888", wp.getUrl());
+    }
+
+    @Test
+    public void testCreatePost() throws Exception {
+        Random r = new Random();
+        String title = "Test" + r.nextInt(1000);
+        String content =  "Test";
+        Post p = new Post(title, content, Post.TYPE.POST, Post.STATUS.PUBLISH);
+        Post newPost = wp.createPost(p);
+        assertEquals(title,newPost.getTitle());
+        assertEquals("post",newPost.getType());
+        assertEquals("publish",newPost.getStatus());
+    }
+
+    @Test
+    public void testCreatePost2() throws Exception {
+        Random r = new Random();
+        String title = "Test" + r.nextInt(1000);
+        String content =  "Test";
+        Post p = new Post(title, content, Post.TYPE.PAGE, Post.STATUS.DRAFT);
+        Post newPost = wp.createPost(p);
+        assertEquals(title,newPost.getTitle());
+        assertEquals("page",newPost.getType());
+        assertEquals("draft",newPost.getStatus());
     }
 }
