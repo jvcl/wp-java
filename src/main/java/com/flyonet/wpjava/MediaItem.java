b1e8d2ca6304f2362277840ba5b1ead8ab79ac6c
jvcl <valdi.jorge@gmail.com>
2015-08-20 13:15:25 -0300

    MediaItem class added

---
+++ src/main/java/com/flyonet/wpjava/MediaItem.java
@@ -0,0 +1,24 @@
+package com.flyonet.wpjava;
+
+/*
+ * This file is part of WP-JAVA.
+ *
+ *  WP-JAVA is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  WP-JAVA is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with WP-JAVA.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * This class represents a media object such as video or image.
+ */
+public class MediaItem {
+}
